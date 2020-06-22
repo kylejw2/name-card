7f87782334f7f30e6edb34d4c0cfae91cc059bb7
kylejw2 <kylejamwright@gmail.com>
2020-06-22 11:47:40 -0600

    Inserted popup

--- event.js
+++ event.js
@@ -1,8 +1,13 @@
 const userText = document.getElementById("user_text");
 const displayArea = document.querySelector("p");
+const btn = document.getElementById("btn");
 
 const updateDisplayArea = (e) => {
     displayArea.innerHTML = userText.value;
 }
+const openPopup = (e) => {
+    alert("Hello, my name is " + displayArea.innerHTML);
+}
 
 userText.addEventListener("input", updateDisplayArea);
+btn.addEventListener("click", openPopup);
